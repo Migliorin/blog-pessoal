eb58c96c27cb239aafdfd36ed699635bd05d887b
Lucas Migliorin <lmdr.eng19@uea.edu.br>
2023-02-08 22:13:24 -0400

    Props nas publicacoes

--- src/components/Publicacoes/Publicacoes.js
+++ src/components/Publicacoes/Publicacoes.js
@@ -2,19 +2,16 @@ import './Publicacoes.css'
 import imgEle from "../../imgs/eletronica.jpeg"
 import { Container } from 'react-bootstrap';
 
-function ImgOverlayExample() {
+function ImgOverlayExample(props) {
   return (
     <Container>
       <div className='publicacao-container'>
-          <img src={imgEle} className='img-publicacao'></img>
-        <h3>Titulo do card</h3>
+          <img src={props.img} className='img-publicacao'></img>
+        <h3>{props.titulo}</h3>
         <div className='data-publicacao'>
-          <p>jan 30, 2023 | <a>Link</a></p>
+          <p>{props.data} | <a>Link</a></p>
         </div>
-        <p className='sumario'>Donec id velit quis orci egestas tincidunt. Proin convallis diam sit amet 
-        ante sagittis tristique. Praesent vitae tristique felis. Pellentesque rutrum ex non porttitor auctor.
-        Nunc in lorem pretium, fermentum enim id, viverra metus. Vivamus eget sem quis tortor cursus pretium 
-        sed et mauris. <a href='' className='read_more'>Read more...</a></p>
+        <p className='sumario'>{props.texto}<a href='' className='read_more'>Read more...</a></p>
         
       </div>
     </Container>
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,6 +2,8 @@ import Paginacao from "../../components/Paginacao/Paginacao";
 import Perfil from "../../components/Perfil/Perfil";
 import Publicacoes from "../../components/Publicacoes/Publicacoes";
 import './HomePage.css';
+import imgEle from "../../imgs/eletronica.jpeg"
+import imgIa from "../../imgs/ia.jpeg"
 
 function HomePage() {
     return (
@@ -11,9 +13,21 @@ function HomePage() {
                 <Perfil />
             </div>
             <div className="container-publicacoes">
-                <Publicacoes />
-                <Publicacoes />
-                <Publicacoes />
+                <Publicacoes 
+                texto="Donec id velit quis orci egestas tincidunt. Proin convallis diam sit amet 
+        ante sagittis tristique. Praesent vitae tristique felis. Pellentesque rutrum ex non porttitor auctor.
+        Nunc in lorem pretium, fermentum enim id, viverra metus. Vivamus eget sem quis tortor cursus pretium 
+        sed et mauris."
+                data="jan 30, 2023"
+                titulo="Teste do card"
+                img={imgEle}
+                />
+                <Publicacoes 
+                texto="A inteligencia artificial avançou bastante desde a ultima decada."
+                data="fev 8, 2023"
+                titulo="Novo teste de card"
+                img={imgIa}
+                />
                 <Paginacao/>
             </div >
         </div>
